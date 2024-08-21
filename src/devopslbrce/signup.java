64134a6a6f25fa74425ccbdab85993ab8c6a024c
AdmiN <AdmiN@ITWS070>
2024-08-21 15:17:47 +0530

    commit

---
+++ src/devopslbrce/signup.java
@@ -0,0 +1,14 @@
+package devopslbrce;
+import java.util.Scanner;
+public class signup {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter username:");
+	String username=sc.nextLine();
+	System.out.print("Enter password:");
+	String password=sc.nextLine();
+	System.out.println("Account created  succesfully");
+	}
+
+}
